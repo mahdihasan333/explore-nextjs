8c57de434f7d1ef6f76be428219b0bfb7e247960
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-25 00:21:12 +0600

    nextjs basic routing

---
+++ src/app/about/address/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddressPage() {
+  return (
+    <div>
+      <p className='font-bold text-3xl'>Address Page</p>
+    </div>
+  )
+}
---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <p className='font-bold text-3xl'>About Page</p>
+    </div>
+  )
+}
