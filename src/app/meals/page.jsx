a04423b041a175ac745037aad36f9acc53299344
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-16 20:13:21 +0600

    added meals page

---
+++ src/app/meals/page.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function MealsPage() {
+    const [meals, setMeals] = useState([])
+    const [search, setSearch] = useState('')
+  return (
+    <div>
+      <p>testing...</p>
+    </div>
+  )
+}
